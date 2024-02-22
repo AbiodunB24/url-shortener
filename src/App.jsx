1a46cf960ad32c9e12f19d82812132d4a07365bb
Babatunde Balogun <balogunbabatunde42@gmail.com>
2024-02-22 13:40:12 +0100

    app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HeroSection from './assets/components/HeroSection'
+import Navbar from './assets/components/Navbar'
+function App() {
+
+  return (
+    <div className='bg-[#0B101B] text-white h-dvh'>
+<Navbar/>
+<HeroSection/>
+    </div>
+  )
+}
+
+export default App
